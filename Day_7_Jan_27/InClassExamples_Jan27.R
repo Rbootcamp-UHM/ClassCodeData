a1f3399911a0c1a792ebc92c894f6d0385768ac9
Marguerite Butler Anolis Pro <mbutler808@gmail.com>
2021-01-27 09:38:21 -1000

    updated files Jan 27

--- Day_7_Jan_27/InClassExamples_Jan27.R
+++ Day_7_Jan_27/InClassExamples_Jan27.R
@@ -14,7 +14,59 @@ paint.iris <- function(iris, sp) {
 	plot(iris[, -5], col=icol)
 	}
 	
-paint.iris(iris, sp)
+paint.iris(iris, iris$Species)
+
+sp <- sample(iris$Species)
+	icol[sp=="setosa"] <- "red"
+	icol[sp=="versicolor"] <-"purple"
+	icol[sp=="virginica"] <- "turquoise"
+	plot(iris[, -5], col=icol)
+
+sp <- sample(iris$Species)
+	icol[sp=="setosa"] <- "red"
+	icol[sp=="versicolor"] <-"purple"
+	icol[sp=="virginica"] <- "turquoise"
+	plot(iris[, -5], col=icol)
+
+sp <- sample(iris$Species)
+	icol[sp=="setosa"] <- "red"
+	icol[sp=="versicolor"] <-"purple"
+	icol[sp=="virginica"] <- "turquoise"
+	plot(iris[, -5], col=icol)
+
+
+paint.iris ( dat=iris[-5], sp=iris$Species )
+
+paint.iris <- function(dat, sp ) {  # paint.iris accepts a dataframe and a species vector
+	
+	spp <- sample(sp)
+	icol[spp=="setosa"] <- "red"
+	icol[spp=="versicolor"] <-"purple"
+	icol[spp=="virginica"] <- "turquoise"
+	plot(dat, col=icol)
+	
+}
+
+paint.iris ( dat=iris[-5], sp=iris$Species )
+
+
+# paint.iris accepts a dataframe and a species vector
+paint.iris <- function(dat=iris[-5], sp=iris$Species ) {  	
+    icol <- as.character(sp)
+	icol[sp=="setosa"] <- "red"
+	icol[sp=="versicolor"] <-"purple"
+	icol[sp=="virginica"] <- "turquoise"
+	plot(dat, col= icol)
+	
+}
+
+paint.iris ( dat=iris[-5], iris$Species )
+
+sp <- sample(iris$Species)
+paint.iris ( dat=iris[-5], sp )
+
+paint.iris ( dat=iris[-5], sample(iris$Species) )
+
 
 ### You want to use generic names inside of functions, because they are just containers for your information. In order to be reusable, it should work on any data that you pass to it
 
@@ -50,12 +102,15 @@ plot(ir[-3], col=sp, pch=16)
 
 
 
-function(dat, sp){ 
+simple.plot <- function(dat=iris[-5], sp=iris$Species, ... ){ 
+  sp <- as.character(sp)
   sp[sp == "a"] <- "red"                   # what can you make generic (= reusable?)
   sp[sp == "b"] <- "blue"
-  plot(dat, col=sp, pch=16)
+  plot(dat, col=sp, ...)
   	
 }
+
+simple.plot( ir[-3], ir$species)
 
 paint.iris( iris[-5],     )   # how is this maximally reusable? 
 
@@ -68,6 +123,34 @@ plot(ir[-3], col=sp, pch=16)
 dat <- data.frame(ir[-3], sp)
 write.csv(dat, file="dat1.csv" )
 
+sp <- sample(sp)
+sp[sp == "a"] <- "red"
+sp[sp == "b"] <- "blue"
+plot(ir[-3], col=sp, pch=16)
+
+dat <- data.frame(ir[-3], sp)
+write.csv(dat, file="dat1.csv" )
+
+
+sp <- sample(sp)
+sp[sp == "a"] <- "red"
+sp[sp == "b"] <- "blue"
+plot(ir[-3], col=sp, pch=16)
+
+dat <- data.frame(ir[-3], sp)
+write.csv(dat, file="dat2.csv" )
+
+
+sp <- sample(sp)
+sp[sp == "a"] <- "red"
+sp[sp == "b"] <- "blue"
+plot(ir[-3], col=sp, pch=16)
+
+dat <- data.frame(ir[-3], sp)
+write.csv(dat, file="dat3.csv" )
+
+
+
 
 	write.csv(dat, file="out1.csv")
 	write.csv(dat, file="out2.csv")
@@ -78,12 +161,64 @@ write.csv(dat, file="dat1.csv" )
 
 files <- paste("out", 1:10, ".csv", sep="")
 
-for (i in   ) {
-	write.csv(dat, file=   )    # how can we replace the filename?
+
+# How to save the output to file?
+
+sp <- sample(sp)
+sp[sp == "a"] <- "red"
+sp[sp == "b"] <- "blue"
+plot(ir[-3], col=sp, pch=16)
+
+dat <- data.frame(ir[-3], sp)
+write.csv(dat, file="dat3.csv" )
+
+out <- dat
+
+for (i in 1:10  ) {
+	
+	write.csv(out, file= files[i])    # how can we replace the filename?
+}
+
+### Can we save the data to save as output after randomizing?
+## The original simple.plot function
+
+simple.plot <- function(dat, sp, ... ){ 
+  sp <- as.character(sp)
+  sp[sp == "a"] <- "red"                   # what can you make generic (= reusable?)
+  sp[sp == "b"] <- "blue"
+  plot(dat, col=sp, ...)
+}
+
+
+### What do we want to save?
+
+simple.plot <- function(dat, sp, ... ){ 
+  sp <- as.character(sp)
+  sp[sp == "a"] <- "red"                   # what can you make generic (= reusable?)
+  sp[sp == "b"] <- "blue"
+  plot(dat, col=sp, ...)
+  dat <- cbind(dat,sp)
+  return(dat)	
+}
+
+
+simple.plot(ir[-3], ir$species)
+
+
+for (i in 1:10  ) {
+	
+	out <- simple.plot(ir[-3], ir$species)
+	write.csv(out, file= files[i])    # how can we replace the filename?
 }
 
 # make a subdirectory and put files in there
+## Make a folder "out" in your working directory
 
+for (i in 1:10  ) {
+	
+	out <- simple.plot(ir[-3], ir$species)
+	write.csv(out, file= paste("out/", files[i], sep=""  ))    # now going to out/out1.csv, etc.
+}
 
 
 
